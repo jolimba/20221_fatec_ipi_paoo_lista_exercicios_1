f69bd3f9a8ee6ed04818557b582f2e5646fb5275
jolimba <joovictors85@gmail.com>
2022-04-13 18:08:44 -0300

    falta a 6

--- 05/05.js
+++ 05/05.js
@@ -1,17 +1,52 @@
 require('dotenv').config()
 const axios = require('axios')
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 const method = process.env.METHOD
 const endpoint = process.env.ENDPOINT
+const appid = process.env.APPID
+const unit = process.env.UNIT
+const url = `${method}://${endpoint}?`
 
-const currentWeather = () => {
 
+const getCurrentWeather = (lat, lon) => {
+    axios
+        .get(`${url}lat=${lat}&lon=${lon}&appid=${appid}&units=${unit}`)
+        .then(result => {
+            console.log(`\nTemperature is ${result.data.main.temp} ºc\n`)
+            currentWeather()
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
 }
 
-readline.question("First number: ", num1 =>{
-    readline.question("Second number: ", num2 => {
-        mathOperations(parseInt(num1), parseInt(num2))
-    })
-})
+const currentWeather = () => {
+    readline.question(`Choose one: 
+        1 - Enter latitude and longitude
+        2 - Exit \n`,
+        choice => {
+            switch(parseInt(choice)) {
+                case 1:
+                    readline.question('Latitude: ', lat => {
+                        readline.question('Longitude: ', lon => {
+                            getCurrentWeather(lat, lon)
+                        })
+                    })
+                    break
+                case 2:
+                    console.log("告別\n")
+                    readline.close()
+                    break
+                default:
+                    console.log('An error has occurred, please try again\n')
+                    currentWeather()
+            }
+        }
+    )
+}
 
-
+currentWeather()
